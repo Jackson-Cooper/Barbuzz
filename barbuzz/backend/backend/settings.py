3639e2fbf54cc543f2ec14250e27710b55a48532
Jackson Cooper <coope5jr@dukes.jmu.edu>
2025-05-07 19:33:16 -0400

    removed stuff

--- barbuzz/backend/backend/settings.py
+++ barbuzz/backend/backend/settings.py
@@ -119,17 +119,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'barbuzz',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'db',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
